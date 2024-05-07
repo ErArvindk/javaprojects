9f8949f88c1241fee52abf40d8bf0b0f474f9d50
ARVIND KUMAR <127136310+ErArvindk@users.noreply.github.com>
2024-05-07 12:58:25 +0530

    Update gradecalculator.java

--- gradecalculator.java
+++ gradecalculator.java
@@ -3,7 +3,7 @@ package project2;
 import java.util.Scanner;
 
 public class gradecalculator {
-    public static void main(string [] args) {
+    public static void main(String[] args) {
         Scanner ar =new Scanner(System.in);
         System.out.print("Enter your total number of subject  = ");
         int numsubject =ar.nextInt();
